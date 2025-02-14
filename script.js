73873a57462e52ca6d92f40b6ba203786c55d331
Federico Silva <fedessilva19@gmail.com>
2025-02-14 12:12:56 -0300

    Arreglo diseño

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ window.addEventListener("scroll", function () {
         navbar.classList.remove("scrolled");
     }
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".scroll-animation");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, { threshold: 0.1 });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
